d6aae5dff0bf422547b0ccca18072cdd8c8fd243
Binod Madhubashitha <binod.16@cse.mrt.ac.lk>
2019-12-20 23:16:51 +0530

    adding loader to profile page

--- src/components/main/Profile.vue
+++ src/components/main/Profile.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="row">
         <div class="col m3 l3"></div>
-        <div class="col s12 m6 l6">
+        <div v-if="!photoURL || !name || !id">
+            <div class="progress">
+                <div class="indeterminate"></div>
+            </div>
+        </div>
+        <div v-else class="col s12 m6 l6">
             <div class="card">
                 <div class="card-image">
                     <img src="../../assets/batch16.jpg">
                     <a class="btn-floating halfway-fab waves-effect waves-light profile-btn left"><img :src="photoURL"/></a>
                 </div>
-                <div v-if="!photoURL || !name || !id">
-                    <div class="progress">
-                        <div class="indeterminate"></div>
-                    </div>
-                </div>
-                <div v-else class="card-content">
+                <div class="card-content">
                     <span class="card-title">{{ name }} ({{ id }})</span>
                     <h6>C-GPA: {{ cgpa }}</h6>
                     <h6>C-Rank: {{ crank }}</h6>
@@ -43,6 +43,9 @@
         created() {
             this.email = firebase.auth().currentUser.email
             this.photoURL = firebase.auth().currentUser.photoURL
+            if(!this.photoURL) {
+                this.photoURL = require('../../assets/user.jpg')
+            }
             db.collection("students").doc(this.email).get().then(doc => {
                 this.cgpa = doc.data().cgpa
                 this.crank = doc.data().crank
--- updater/test.py
+++ updater/test.py
@@ -7,8 +7,24 @@ cred = credentials.Certificate('serviceAccount.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-students_ref = db.collection(u'students')
-modules_ref = db.collection(u'modules').order_by(u'semester')
+# students_ref = db.collection(u'students')
+# modules_ref = db.collection(u'modules').order_by(u'semester')
 
-for module in modules_ref.stream():
-  print(str(module.get('semester')) + '\t' + module.id + '\t' + str(module.get('gpa')) + '\t' + str(module.get('credits')) + '\t' + module.get('title'))
+student_ref = db.collection(u'students').document(u'haritha@cse.mrt.ac.lk')
+student_doc = student_ref.get()
+student = student_doc.to_dict()
+
+data = {
+    u'cgpa': student['cgpa'],
+    u'crank': student['crank'],
+    u'email': u'haritha.16@cse.mrt.ac.lk',
+    u'field': student['field'],
+    u'id': student['id'],
+    u'mobile': student['mobile'],
+    u'name': student['name'],
+    u'results': student['results'],
+    u'sgpa': student['sgpa'],
+    u'srank': student['srank'],
+}
+print(data)
+db.collection(u'students').document(u'haritha.16@cse.mrt.ac.lk').set(data)
